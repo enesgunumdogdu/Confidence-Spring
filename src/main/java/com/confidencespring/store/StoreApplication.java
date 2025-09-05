260e280f36a65ebcf00ba4b0d2d9851e09486f68
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-09-05 22:13:38 +0300

    spring boot starter dependency added.

--- src/main/java/com/confidencespring/store/StoreApplication.java
+++ src/main/java/com/confidencespring/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.confidencespring.store;
 
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class StoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(StoreApplication.class, args);
     }
-
 }
